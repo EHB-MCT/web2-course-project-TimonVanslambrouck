e078dd5b302a2b004fc3adefb8ff74f2d31060f6
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2020-11-26 11:16:22 +0100

    video 8.2 clean

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     async function runTest() {
-        const resp = await fetch("http://localhost:3000/api/pokemon");
+        const resp = await fetch("https://web2-course-project-api-tv.herokuapp.com/api/pokemon");
         const data = await resp.json();
         document.getElementById('content').innerText = JSON.stringify(data);
         console.log(data);
